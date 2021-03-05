feed7d53b74ea4221651a48e090c442eaa87917d
Joca <me@joca.dev>
2021-03-05 13:26:04 +0100

    Update legal route page

--- pages/[lang]/legal.js
+++ pages/[lang]/legal.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Head from "next/head"
 import { useTranslation } from 'react-i18next'
 
-import Legal from 'components/legal'
+import Legal from 'components/legalInfo'
 import Footer from 'components/footer'
 
 const LegalPage = () => {
